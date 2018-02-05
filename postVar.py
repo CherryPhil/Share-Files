8e4082b43885297d3840c5926620c33748fe98b4
CherryPhil <32829480+CherryPhil@users.noreply.github.com>
2018-02-05 21:05:47 +0800

    Add files via upload

--- postVar.py
+++ postVar.py
@@ -1,20 +1,21 @@
+
 from wtforms import Form, StringField, SubmitField, SelectField, TextAreaField, IntegerField, validators
 
 class Post(Form):
     title = StringField('Title',[validators.DataRequired()])
     text = TextAreaField('Text', [validators.DataRequired()])
-    submit = SubmitField('Post', [validators.DataRequired()])
+    submit = SubmitField('', [validators.DataRequired()])
 
 class Announcement(Form):
     announcement = StringField('Announcement', [validators.DataRequired()])
     content = TextAreaField('Content', [validators.DataRequired()])
-    submit = SubmitField('Submit', [validators.DataRequired()])
+    submit = SubmitField('', [validators.DataRequired()])
 
 class Contact(Form):
     email = StringField('Email Address:', [validators.DataRequired()])
     subject = StringField('Subject:', [validators.DataRequired()])
     message = TextAreaField('Your message:', [validators.DataRequired()])
-    submit = SubmitField('Submit', [validators.DataRequired()])
+    submit = SubmitField('', [validators.DataRequired()])
 
 class User_recipe(Form):
     name = StringField('Food Name', [validators.DataRequired()])
@@ -24,4 +25,4 @@ class User_recipe(Form):
     calories = IntegerField('Calories', [validators.DataRequired()])
     ingredients = StringField('Ingredients', [validators.DataRequired()])
     recipes = TextAreaField('Recipes', [validators.DataRequired()])
-    submit = SubmitField('Post', [validators.DataRequired()])
+    submit = SubmitField('', [validators.DataRequired()])
